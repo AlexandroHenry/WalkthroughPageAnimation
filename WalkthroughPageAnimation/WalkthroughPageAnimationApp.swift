c1339f7aaf330cc1d57f43cb6ffa68612a9ea40e
alaxhenry <alaxhenry0121@gmail.com>
2022-12-16 11:41:02 +0900

    Initial Commit

---
+++ WalkthroughPageAnimation/WalkthroughPageAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughPageAnimationApp.swift
+//  WalkthroughPageAnimation
+//
+//  Created by Seungchul Ha on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct WalkthroughPageAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
